4283df1880c09f93a2d2e2cef03808be52b1497f
Timothy Oliver <oliverwakogi@live.com>
2019-11-18 16:24:35 +0300

    Connect application to postgres

--- tenebrous/settings.py
+++ tenebrous/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'tenebrous.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD')
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
