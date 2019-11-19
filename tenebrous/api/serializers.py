65eb7e047ce395c3ef9c0fe72f96730e6c3e427d
Timothy Oliver <oliverwakogi@live.com>
2019-11-19 08:56:38 +0300

    Implement register functionality

--- tenebrous/api/serializers.py
+++ tenebrous/api/serializers.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token 
+from rest_framework.response import Response
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = User
-    fields = ['url', 'username', 'email', 'groups']
+    fields = ['id', 'username', 'email', 'password']
+    extra_kwargs = {
+      'password': {
+        'write_only': True,
+        'required': True
+      }
+    }
+  
+  def create(self, validated_data):
+    user = User.objects.create_user(**validated_data)
+    token = Token.objects.create(user=user)
+    return Response({ 'token': token.key })
